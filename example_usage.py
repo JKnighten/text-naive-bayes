fd5382655d442e438e838be043706897f1716b94
Johnny Knighten <jknigh28@gmail.com>
2018-12-06 21:13:44 -0500

    Updated Wine Data Parse Prototype Notebook To Make Vectorized Data Updated nltk_wine_data.py To Create Dictionary And Return Vector Data Updated example_usage.py

--- example_usage.py
+++ example_usage.py
@@ -9,20 +9,15 @@ from naivebayes.models.dictionary import Multinomial
 with open('./sample_data/nltk/wine/wine_data.pkl', 'rb') as f:
         wine_data = pickle.load(f)
 
-# Remove Top N Most Common Words
-n = 50
-label_0_top_50 = {word for word, freq in wine_data[0][0].most_common(n)}
-label_1_top_50 = {word for word, freq in wine_data[0][1].most_common(n)}
+wine_bow_data = wine_data["bagofwords"]
 
-for word_to_remove in label_0_top_50.union(label_1_top_50):
-    del wine_data[0][0][word_to_remove]
-    del wine_data[0][1][word_to_remove]
+freq_data = wine_bow_data["freq_data"]
+label_counts = wine_bow_data["label_counts"]
+raw_data = wine_bow_data["raw_data"]
+labels = wine_bow_data["labels"]
 
 model = Multinomial()
-model.train(wine_data[1], wine_data[0])
+model.train(label_counts, freq_data)
 
-print(model.predict(wine_data[2]))
-print(wine_data[3])
-
-
-
+print(model.predict(raw_data))
+print(labels)
--- sample_data/nltk/wine/nltk_wine_data.py
+++ sample_data/nltk/wine/nltk_wine_data.py
@@ -1,6 +1,9 @@
+from functools import reduce
+
 import nltk
 import string
 import pickle
+import numpy as np
 
 # Download WebText Corpus If Not Already Downloaded
 nltk.download('webtext')
@@ -13,8 +16,12 @@ wine_reviews_raw = webtext.raw("wine.txt").split("\n")
 # Used To Remove Non-Text Data
 translator = str.maketrans('', '', string.punctuation)
 
-cleaned_review_data = []
 
+################
+# Bag Of Words #
+################
+
+cleaned_review_data = []
 review_labels = []
 label_bag_of_words = ["", ""]
 for review in wine_reviews_raw:
@@ -62,6 +69,15 @@ freq_dist_0 = nltk.FreqDist(label_0_tokens)
 label_1_tokens = nltk.tokenize.word_tokenize(label_bag_of_words[1])
 freq_dist_1 = nltk.FreqDist(label_1_tokens)
 
+# Remove Top N Frequent Words
+n = 25
+label_0_top_50 = {word for word, freq in freq_dist_0.most_common(n)}
+label_1_top_50 = {word for word, freq in freq_dist_1.most_common(n)}
+
+for word_to_remove in label_0_top_50.union(label_1_top_50):
+    del freq_dist_0[word_to_remove]
+    del freq_dist_1[word_to_remove]
+
 # Make Dictionary Containing Frequency Distribution For Each Label
 wine_freq_data = {}
 wine_freq_data[0] = freq_dist_0
@@ -72,6 +88,41 @@ label_counts = {}
 label_counts[0] = len(list(filter(lambda label: label == 0, review_labels)))
 label_counts[1] = len(list(filter(lambda label: label == 1, review_labels)))
 
+
+###################
+# Vectorized Data #
+###################
+
+dictionary = reduce(lambda key_a, key_b: set(wine_freq_data[key_a].keys()).union(set(wine_freq_data[key_b].keys())),
+                    wine_freq_data)
+
+data_as_vectors = np.zeros((len(wine_reviews_raw), len(dictionary)))
+
+# Create map
+word_map = {}
+for i, key in enumerate(dictionary):
+    word_map[key] = i
+
+# Create Vectors
+for i, review in enumerate(wine_reviews_raw):
+
+    if review_labels[i] == 0:
+
+        # Remove "No Stars" From Reviews With 0 Stars
+        if "No Stars" in review:
+            review = review.replace('No Stars','')
+
+    words = nltk.tokenize.word_tokenize(review.lower().translate(translator))
+
+    for word in words:
+        if word in word_map:
+            data_as_vectors[i, word_map[word]] += 1
+
+
+final_data = {"bagofwords": {"freq_data": wine_freq_data, "label_counts": label_counts, "raw_data": cleaned_review_data,
+                             "labels": review_labels},
+              "vectors": {"vectors": data_as_vectors, "word_map": word_map}}
+
 # Write Pickle File
 with open('wine_data.pkl', 'wb') as f:
-        pickle.dump([wine_freq_data, label_counts, cleaned_review_data, review_labels], f, pickle.HIGHEST_PROTOCOL)
+        pickle.dump(final_data, f, pickle.HIGHEST_PROTOCOL)
