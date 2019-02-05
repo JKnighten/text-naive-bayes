7fafaefe730d5fed9e7ce9715aba12037a4a94d9
Johnny Knighten <jknigh28@gmail.com>
2019-02-05 17:59:49 -0500

    Added Unittest For Dictionary Based Multinomial NB Fixed Error In Calaculating Likelihoods In Dictionary Based Multinomial NB Updated Variable Names In Dictionary Multinomial NB Cleaned Up example_usage.py

--- example_usage.py
+++ example_usage.py
@@ -7,17 +7,25 @@ from naivebayes.models.dictionary import Multinomial
 #####################
 
 with open('./sample_data/nltk/wine/wine_data.pkl', 'rb') as f:
-        wine_data = pickle.load(f)
+    wine_data = pickle.load(f)
 
-wine_bow_data = wine_data["bagofwords"]
+    wine_bow_data = wine_data["bagofwords"]
 
-freq_data = wine_bow_data["freq_data"]
-label_counts = wine_bow_data["label_counts"]
-raw_data = wine_bow_data["raw_data"]
-labels = wine_bow_data["labels"]
+    freq_data = wine_bow_data["freq_data"]
+    label_counts = wine_bow_data["label_counts"]
+    raw_data = wine_bow_data["raw_data"]
+    labels = wine_bow_data["labels"]
 
-model = Multinomial()
-model.train(label_counts, freq_data)
+    model = Multinomial()
+    model.train(label_counts, freq_data)
 
-print(model.predict(raw_data))
-print(labels)
+    predictions = model.predict(raw_data)
+
+    matches = 0
+    for i in range(0, len(labels)):
+        if predictions[i] == labels[i]:
+            matches += 1
+
+    print("Wine Data Example")
+    print("Accuracy - " + str(matches/len(labels)))
+
--- naivebayes/models/dictionary.py
+++ naivebayes/models/dictionary.py
@@ -2,63 +2,67 @@ from functools import reduce
 
 
 class Multinomial:
-
     def __init__(self, smoothing=1.0):
         self.smoothing = smoothing
-        self.labels_counts = None
+        self.label_counts = None
         self.train_data = None
         self.dictionary = None
-        self.smoothed_params = {}
-        self.smoothed_params_empty = {}
-        self.label_probs = {}
+        self.smoothed_likelihood = {}
+        self.smoothed_likelihood_empty = {}
+        self.label_prior = {}
 
     def train(self, label_counts, train_data):
-        self.labels_counts = label_counts
+        self.label_counts = label_counts
         self.train_data = train_data
 
-        # Create Global Dictionary
+        # Create Dictionary Using All Labels
         self.dictionary = \
             reduce(lambda key_a, key_b: set(self.train_data[key_a].keys()).union(set(self.train_data[key_b].keys())),
                    self.train_data)
 
         # Get Total Number Of Training Instances
-        numb_training_instances = reduce(lambda key_a, key_b: self.labels_counts[key_a] + self.labels_counts[key_b],
-                           self.labels_counts)
+        numb_training_instances = sum(self.label_counts.values())
 
-        # Smooth Params
-        for label in train_data:
+        # Calculate Likelihoods And Apply Smoothing
+        for label in self.train_data:
 
-            # Number Of Times Label Occurred
-            label_word_count = self.labels_counts[label]
+            # Number Of Words Belonging To Current Label
+            label_word_count = sum(self.train_data[label].values())
 
-            # Calculate Smoothed Param For Each Word In Label
-            label_smoothed_params = {}
+            # Calculate Smoothed Likelihood For Each Word In Label
+            label_likelihoods = {}
             for word in train_data[label]:
-                label_smoothed_params[word] = (self.train_data[label][word] + self.smoothing) /\
-                                                    (label_word_count + self.smoothing*len(self.dictionary))
+                label_likelihoods[word] = (self.train_data[label][word] + self.smoothing) / \
+                                          (label_word_count + self.smoothing * len(self.dictionary))
 
-            self.smoothed_params[label] = label_smoothed_params
-            self.smoothed_params_empty[label] = self.smoothing / (label_word_count + self.smoothing*len(self.dictionary))
+            self.smoothed_likelihood[label] = label_likelihoods
+            self.smoothed_likelihood_empty[label] = self.smoothing / \
+                                                    (label_word_count + self.smoothing * len(self.dictionary))
 
-            self.label_probs[label] = self.labels_counts[label] / numb_training_instances
+            # Label Prior Calculation
+            self.label_prior[label] = self.label_counts[label] / numb_training_instances
 
     def predict(self, test_data):
 
         predicted_labels = []
 
+        # Make A Prediction For Each List of Words in test_data
         for word_list in test_data:
 
             scores = {}
 
+            # Initialize By Putting Priors In First
             for label in self.train_data.keys():
-                scores[label] = self.label_probs[label]
+                scores[label] = self.label_prior[label]
 
+            # For Each Word Update Score With Smoothed Likelihood
             for word in word_list:
-
+                # Skip Words Not In Dictionary
                 if word in self.dictionary:
                     for label in self.train_data.keys():
-                        scores[label] *= self.smoothed_params[label].get(word, self.smoothed_params_empty[label])
-
+                        scores[label] *= self.smoothed_likelihood[label].get(word,
+                                                                             self.smoothed_likelihood_empty[label])
+            # Append Label With Highest Score To Output
             predicted_labels.append(max(scores, key=scores.get))
 
         return predicted_labels
